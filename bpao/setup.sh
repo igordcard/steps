2724974d35c2b699c68d0197328a7438fc3a3960
Igor D.C <igordcard@gmail.com>
2020-04-27 19:26:33 -0700

    Committing changes from last week
    Next step is to try bpao with real BMHs, since this dev setup
    is likely missing something that Itohan was using in addition.

--- bpao/setup.sh
+++ bpao/setup.sh
@@ -24,26 +24,50 @@ tar -xf vagrant-e2e-20200420.tar.gz
 cd vagrant_e2e
 kubectl create -f "https://raw.githubusercontent.com/metal3-io/baremetal-operator/master/deploy/crds/metal3.io_baremetalhosts_crd.yaml"
 ./setup.sh -p libvirt
+vagrant plugin install vagrant-proxyconf
 
-#./bpa_e2e_test.sh
+# the following comes from bpa_e2e_test.sh
 apt-get update
 apt-get install -y vagrant virtualbox
 cp fake_dhcp_lease /opt/icn/dhcp/dhcpd.leases # this is where ICN looks for dhcp leases
 kubectl apply -f bmh-bpa-test.yaml
-cat /root/.ssh/id_rsa.pub > vm_authorized_keys
+cat /root/.ssh/id_rsa.pub > vm_authorized_keys # define proxy through environment even if chameleonsocks already running
+# this is because the vagrant vm will not use chameleonsocks by default
+export http_proxy=http://proxy.company.com:911
+export https_proxy=http://proxy.company.com:911
+export no_proxy=localhost,127.0.0.1
+export HTTP_PROXY=$http_proxy
+export HTTPS_PROXY=$https_proxy
+export NO_PROXY=$no_proxy
 vagrant up
+unset http_proxy
+unset https_proxy
+unset HTTP_PROXY
+unset HTTPS_PROXY
+unset no_proxy
+unset NO_PROXY
 sleep 5
 ssh-keygen -f "/root/.ssh/known_hosts" -R "192.168.50.63"
 kubectl apply -f e2e_test_provisioning_cr.yaml
-
-kubectl apply -f e2e_test_software_cr.yaml # added by mew
+kubectl apply -f e2e_test_software_cr.yaml # added by me
 
 # access VM
 # vagrant ssh bpa-test-vm -- -i ~/.ssh/id_rsa
 
+# 20200423: THIS DOESN'T ACTUALLY WORK, DNS ISSUES INSIDE VAGRANT VM with docker
 
 # uninstall
 make bpa_op_delete
 kubectl delete secret ssh-key-secret
 # and others
+#kubectl delete secret ssh-key-secret # created by make bpa_op_install
+kubectl delete -f e2e_test_software_cr.yaml
+kubectl delete -f e2e_test_provisioning_cr.yaml
+kubectl delete job kud-cluster-test
+#kubectl delete deployment bpa-operator
+vagrant destroy -f
+#virsh destroy vagrant_e2e_bpa-test*
+#virsh undefine vagrant_e2e_bpa-test*
+# and others
+cd ~/icn
 make bpa_op_delete
