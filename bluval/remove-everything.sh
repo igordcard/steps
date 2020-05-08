2f953818d62464d9753463cc1269cda145ba13ef
Igor D.C <igordcard@gmail.com>
2020-05-08 15:48:44 -0700

    Properly deleting kubespray should delete docker too
    Otherwise docker network connectivity might become messed up.

--- bluval/remove-everything.sh
+++ bluval/remove-everything.sh
@@ -8,3 +8,6 @@ sudo make kud_bm_reset
 # remove kubespray from multicloud-k8s:
 cd k8s/kud/hosting_providers/vagrant
 ansible-playbook -i inventory/hosts.ini /opt/kubespray-2.12.6/reset.yml --become --become-user=root -e reset_confirmation=yes 
+# do the following on all of the k8s nodes:
+docker rmi -f $(docker image ls -a -q)
+sudo apt-get purge docker-* -y --allow-change-held-packages
