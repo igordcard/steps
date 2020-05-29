e5b5b8b83ed40e6030cb5c400f6a6de2fd4b999a
Igor D.C <igordcard@gmail.com>
2020-05-28 18:54:17 -0700

    Fix kube-hunter better
    Additional changes to anything bluval likely will only go to the wiki.

--- bluval/bluval-fixes.sh
+++ bluval/bluval-fixes.sh
@@ -1,3 +1,4 @@
+# Fix kube-hunter
 kubectl replace -f - << EOF
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRole
@@ -12,6 +13,15 @@ rules:
 - nonResourceURLs:
   - /livez
   - /readyz
+  - /healthz
   verbs:
   - get
+EOF
+
+kubectl replace -f - << EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: default
+automountServiceAccountToken: false
 EOF
