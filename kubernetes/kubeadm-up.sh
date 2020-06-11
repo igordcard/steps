17f3989ee73c0a401f5e16ac47c23c5bffe134be
Igor D.C <igordcard@gmail.com>
2020-06-10 20:38:00 -0700

    Move docker installation steps to its own script

--- kubernetes/kubeadm-up.sh
+++ kubernetes/kubeadm-up.sh
@@ -3,19 +3,6 @@
 # bionic
 
 cd
-sudo su -c "cat > /etc/modules-load.d/containerd.conf <<EOF
-overlay
-br_netfilter
-EOF"
-
-sudo modprobe overlay
-sudo modprobe br_netfilter
-
-sudo su -c "cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-EOF"
 
 sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
 sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
@@ -25,35 +12,8 @@ sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
-# install docker-ce manually
-
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-sudo apt-mark unhold docker-ce docker-ce-cli
-sudo apt-get install -y docker-ce=5:18.09.9~3-0~ubuntu-bionic docker-ce-cli=5:18.09.9~3-0~ubuntu-bionic containerd.io build-essential
-sudo apt-mark hold docker-ce docker-ce-cli
-
-sudo su -c 'cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF'
-
-sudo mkdir -p /etc/systemd/system/docker.service.d
-
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-
-sudo usermod -a -G docker $USER
+# install docker-ce manually using install-docker.sh
+# #######
 
 # k8s setup
 
