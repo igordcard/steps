02f754f7e44ca2c14cedcf3d4fcbcc7525389109
Igor D.C <igordcard@gmail.com>
2020-06-19 00:32:46 -0700

    Install Istio with replicated
    But nothing configured yet.

--- emco/dcm/istio.sh
+++ emco/dcm/istio.sh
@@ -4,8 +4,6 @@
 
 # setup vagrant VMs
 
-
-
 # # if k8s already running for emco, run this first:
 # kubectl delete job kud-cluster-101
 # kubectl delete job kud-cluster-102
@@ -18,3 +16,16 @@
 
 # let's use whatever got deployed by emco kud job and put istio on top of it
 
+########### do the following in each cluster ###########
+export ISTIO_VERSION=1.6.3
+curl -L https://istio.io/downloadIstio | sh -
+cd istio-$ISTIO_VERSION
+export PATH=$PWD/bin:$PATH
+kubectl create namespace istio-system
+kubectl create secret generic cacerts -n istio-system \
+    --from-file=samples/certs/ca-cert.pem \
+    --from-file=samples/certs/ca-key.pem \
+    --from-file=samples/certs/root-cert.pem \
+    --from-file=samples/certs/cert-chain.pem
+istioctl install \
+    -f manifests/examples/multicluster/values-istio-multicluster-gateways.yaml
