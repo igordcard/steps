2f5bc6541b4c97741c9cbfd23a82aecef1c6145f
Igor D.C <igordcard@gmail.com>
2020-07-13 13:44:33 -0700

    Add vagrant VMs IP addrs vars to bashrc

--- emco/dcm/bring-up-vms.sh
+++ emco/dcm/bring-up-vms.sh
@@ -15,6 +15,8 @@ sed -i "s/cpus = 16/cpus = 12/" Vagrantfile
 sed -i "s/size = 400/size = 100/" Vagrantfile
 vagrant up
 export VAGRANT_IP_ADDR1=$(vagrant ssh-config | grep HostName | cut -f 4 -d " ")
+sed -i '/VAGRANT_IP_ADDR1/d' ~/.bashrc
+echo "export VAGRANT_IP_ADDR1=$VAGRANT_IP_ADDR1" >> ~/.bashrc
 ssh-copy-id -f -i ~/.ssh/id_rsa.pub -o "IdentityFile .vagrant/machines/default/libvirt/private_key" -o StrictHostKeyChecking=no vagrant@$VAGRANT_IP_ADDR1
 ssh vagrant@$VAGRANT_IP_ADDR1 -t "sudo su -c 'mkdir /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/'"
 cp Vagrantfile ../testing2/Vagrantfile
@@ -23,6 +25,8 @@ cd ../testing2
 sed -i "s/\"cluster-101\"/\"cluster-102\"/" Vagrantfile
 vagrant up
 export VAGRANT_IP_ADDR2=$(vagrant ssh-config | grep HostName | cut -f 4 -d " ")
+sed -i '/VAGRANT_IP_ADDR2/d' ~/.bashrc
+echo "export VAGRANT_IP_ADDR2=$VAGRANT_IP_ADDR2" >> ~/.bashrc
 ssh-copy-id -f -i ~/.ssh/id_rsa.pub -o "IdentityFile .vagrant/machines/default/libvirt/private_key" -o StrictHostKeyChecking=no vagrant@$VAGRANT_IP_ADDR2
 ssh vagrant@$VAGRANT_IP_ADDR2 -t "sudo su -c 'mkdir /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/'"
 cd
