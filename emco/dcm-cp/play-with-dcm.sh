62c20eefb7c83615a5c3db0f3be553c366f478dc
Igor D.C <igordcard@gmail.com>
2020-07-18 23:46:15 -0700

    Test DCM API

--- emco/dcm-cp/play-with-dcm.sh
+++ emco/dcm-cp/play-with-dcm.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-curl localhost:9015/v2/projects/test-project/ # doesn't work - what creates the project?
+# Create test-project Project
+cd ~/multicloud-k8s/src/orchestrator
+./orchestrator &
+cat > create-project.json << EOF
+{"metadata": {"name": "test-project"}}
+EOF
+curl --header "Content-Type: application/json" --request POST --data @create-project.json http://127.0.0.1:9015/v2/projects
+
+# Test DCM API
+cd ~/multicloud-k8s/src/dcm
+./dcm &
+cd ~/multicloud-k8s/src/dcm/test
+./dcm_call_api.sh
