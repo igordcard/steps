0da097fe2417d6975408f94bda18ff9f319b1481
Igor D.C <igordcard@gmail.com>
2020-07-25 08:27:07 -0700

    This is how I want the config.jsons to look like

--- emco/dcm-cp/build-dcm-for-dev.sh
+++ emco/dcm-cp/build-dcm-for-dev.sh
@@ -9,17 +9,52 @@ cd $WORKDIR/src/ovnaction && make all
 cd $WORKDIR/src/clm && make all
 cd $WORKDIR/src/dcm && make all
 
-cp $WORKDIR/deployments/helm/v2/onap4k8s/orchestrator/resources/config/config.json $WORKDIR/src/orchestrator/
-cp $WORKDIR/deployments/helm/v2/onap4k8s/ncm/resources/config/config.json $WORKDIR/src/ncm/
-cp $WORKDIR/deployments/helm/v2/onap4k8s/rsync/resources/config/config.json $WORKDIR/src/rsync/
-cp $WORKDIR/deployments/helm/v2/onap4k8s/ovnaction/resources/config/config.json $WORKDIR/src/ovnaction/
-cp $WORKDIR/deployments/helm/v2/onap4k8s/clm/resources/config/config.json $WORKDIR/src/clm/
+cat > $WORKDIR/src/orchestrator/config.json << EOF
+{
+    "database-type": "mongo",
+    "database-ip": "172.18.0.2",
+    "etcd-ip": "172.18.0.3",
+    "service-port": "9015"
+}
+EOF
+cat > $WORKDIR/src/ncm/config.json << EOF
+{
+    "database-type": "mongo",
+    "database-ip": "172.18.0.2",
+    "etcd-ip": "172.18.0.3",
+    "service-port": "9031"
+}
+EOF
+cat > $WORKDIR/src/rsync/config.json << EOF
+{
+    "database-type": "mongo",
+    "database-ip": "172.18.0.2",
+    "etcd-ip": "172.18.0.3",
+    "service-port": "9017"
+}
+EOF
+cat > $WORKDIR/src/ovnaction/config.json << EOF
+{
+    "database-type": "mongo",
+    "database-ip": "172.18.0.2",
+    "etcd-ip": "172.18.0.3",
+    "service-port": "9051"
+}
+EOF
+cat > $WORKDIR/src/clm/config.json << EOF
+{
+    "database-type": "mongo",
+    "database-ip": "172.18.0.2",
+    "etcd-ip": "172.18.0.3",
+    "service-port": "9061"
+}
+EOF
 cat > $WORKDIR/src/dcm/config.json << EOF
 {
     "database-ip": "172.18.0.2",
     "database-type": "mongo",
     "plugin-dir": "plugins",
-    "service-port": "9015",
+    "service-port": "9077",
     "ca-file": "ca.cert",
     "server-cert": "server.cert",
     "server-key": "server.key",
