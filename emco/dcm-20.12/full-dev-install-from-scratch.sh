d21f409586613b19ebbd826afed6d9a7e870edcd
Igor D.C <igordcard@gmail.com>
2020-11-16 15:54:28 -0800

    Now able to deploy the 2 clusters for EMCO

--- emco/dcm-20.12/full-dev-install-from-scratch.sh
+++ emco/dcm-20.12/full-dev-install-from-scratch.sh
@@ -2,43 +2,100 @@
 
 # run as root
 
-EMCO_REMOTE="https://github.com/onap/multicloud-k8s.git"
-EMCO_DIR=~/multicloud-k8s
+MK8S_REMOTE="https://github.com/onap/multicloud-k8s.git"
+MK8S_DIR=~/multicloud-k8s
 
-git clone $EMCO_REMOTE
+git clone $MK8S_REMOTE
 
 apt-get install vagrant -y
+$MK8S_DIR/kud/hosting_providers/vagrant/setup.sh -p libvirt
 
-# prepare the vagrant vms for the cluster:
-cd $EMCO_DIR/kud/hosting_providers/containerized
-cp -R testing testing2
-cd testing
-sed -i "s/\"ubuntu18\"/\"cluster-101\"/" Vagrantfile
-sed -i "s/memory = 32768/memory = 20480/" Vagrantfile
-sed -i "s/cpus = 16/cpus = 12/" Vagrantfile
-sed -i "s/size = 400/size = 100/" Vagrantfile
+# template sample vagrant clusters:
+cd $MK8S_DIR/kud/hosting_providers/containerized/
+cp -R testing cluster01
+cp -R testing cluster02
+cp -R testing cluster03
+
+# prepare the 1st vagrant VM cluster:
+cd $MK8S_DIR/kud/hosting_providers/containerized/cluster01
+sed -i "s/\"ubuntu18\"/\"cluster01\"/" Vagrantfile
+sed -i "s/memory = 32768/memory = 10248/" Vagrantfile
+sed -i "s/cpus = 16/cpus = 2/" Vagrantfile
+sed -i "s/size = 400/size = 30/" Vagrantfile
 vagrant up
+
 export VAGRANT_IP_ADDR1=$(vagrant ssh-config | grep HostName | cut -f 4 -d " ")
 sed -i '/VAGRANT_IP_ADDR1/d' ~/.bashrc
 echo "export VAGRANT_IP_ADDR1=$VAGRANT_IP_ADDR1" >> ~/.bashrc
 ssh-copy-id -f -i ~/.ssh/id_rsa.pub -o "IdentityFile .vagrant/machines/default/libvirt/private_key" -o StrictHostKeyChecking=no vagrant@$VAGRANT_IP_ADDR1
 ssh vagrant@$VAGRANT_IP_ADDR1 -t "sudo su -c 'mkdir /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/'"
-cp Vagrantfile ../testing2/Vagrantfile
 
-cd ../testing2
-sed -i "s/\"cluster-101\"/\"cluster-102\"/" Vagrantfile
+# SEE(INSIDE-VAGRANT)
+
+# prepare the 2nd vagrant VM cluster:
+cd $MK8S_DIR/kud/hosting_providers/containerized/cluster02
+sed -i "s/\"ubuntu18\"/\"cluster02\"/" Vagrantfile
+sed -i "s/memory = 32768/memory = 10248/" Vagrantfile
+sed -i "s/cpus = 16/cpus = 2/" Vagrantfile
+sed -i "s/size = 400/size = 30/" Vagrantfile
 vagrant up
+
 export VAGRANT_IP_ADDR2=$(vagrant ssh-config | grep HostName | cut -f 4 -d " ")
 sed -i '/VAGRANT_IP_ADDR2/d' ~/.bashrc
 echo "export VAGRANT_IP_ADDR2=$VAGRANT_IP_ADDR2" >> ~/.bashrc
 ssh-copy-id -f -i ~/.ssh/id_rsa.pub -o "IdentityFile .vagrant/machines/default/libvirt/private_key" -o StrictHostKeyChecking=no vagrant@$VAGRANT_IP_ADDR2
 ssh vagrant@$VAGRANT_IP_ADDR2 -t "sudo su -c 'mkdir /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/'"
-cd
+
+# SEE(INSIDE-VAGRANT)
+
+#####
 
 # if vars lost:
 cd
-pushd ~/multicloud-k8s/kud/hosting_providers/containerized/testing
+cd $MK8S_DIR/kud/hosting_providers/containerized/cluster01
 export VAGRANT_IP_ADDR1=$(vagrant ssh-config | grep HostName | cut -f 4 -d " ")
-cd ../testing2
+cd $MK8S_DIR/kud/hosting_providers/containerized/cluster02
 export VAGRANT_IP_ADDR2=$(vagrant ssh-config | grep HostName | cut -f 4 -d " ")
-popd
+
+
+# ===========================
+# inside each of the clusters
+# REF(INSIDE-VAGRANT)
+
+ssh root@$VAGRANT_IP_ADDR1
+
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+MK8S_REMOTE="https://github.com/onap/multicloud-k8s.git"
+MK8S_DIR=~/multicloud-k8s
+
+git clone $MK8S_REMOTE
+cd $MK8S_DIR
+
+# install kubernetes/docker using KUD AIO (global cluster):
+sed -i 's/^localhost/$HOSTNAME/' kud/hosting_providers/baremetal/aio.sh
+
+# >> disable addons:
+vim kud/hosting_providers/vagrant/installer.sh
+# comment the following lines:
+
+#install_addons
+#if ${KUD_PLUGIN_ENABLED:-false}; then
+#    install_plugin
+#fi
+
+# deploy KUD
+kud/hosting_providers/baremetal/aio.sh
+
+exit
+
+#####
+
+# repeat the above but for:
+ssh root@$VAGRANT_IP_ADDR2
+# SEE(INSIDE-VAGRANT)
+
+# ===========================
+# back out to the main host
+# REF(INSTALL-EMCO)
+
