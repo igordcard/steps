ea82f0b40a82913ef58b5648e6920d72bc0b540e
Igor D.C <igordcard@gmail.com>
2022-04-08 11:29:56 -0700

    Quick steps to run after rebooting my emco dev env

---
+++ emco/22.06/after-reboot.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+docker start root_etcd_1
+docker start deployments_mongo_1
+
+cd /root/vagrant/k23-1
+vagrant up
+cd /root/vagrant/k22-1
+vagrant up
+
+# restart all EMCO services (usually in tmux)
+EMCO_DIR=~/emco-base
+cd $EMCO_DIR/bin/orchestrator
+killall orchestrator
+./orchestrator >> log.txt 2>&1 &
+sleep 2 # give it a bit time to make sure orchestrator creates referential integrity db resources before other services can run
+cd $EMCO_DIR/bin/rsync
+killall rsync
+./rsync >> log.txt 2>&1 &
+cd $EMCO_DIR/bin/clm
+killall clm
+./clm >> log.txt 2>&1 &
+cd $EMCO_DIR/bin/dcm
+killall dcm
+./dcm >> log.txt 2>&1 &
+cd $EMCO_DIR/bin/ncm
+killall ncm
+./ncm >> log.txt 2>&1 &
+cd $EMCO_DIR/bin/ovnaction
+killall ovnaction
+./ovnaction >> log.txt 2>&1 &
+cd $EMCO_DIR/bin/dtc
+killall dtc
+./dtc >> log.txt 2>&1 &
+cd $EMCO_DIR/bin/genericactioncontroller
+killall genericactioncontroller
+./genericactioncontroller >> log.txt 2>&1 &
+
+hardreset_emco
--- emco/22.06/bashrc-functions.sh
+++ emco/22.06/bashrc-functions.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 hardreset_emco() {
-	ns="standard-lc-ns"
-	killall dcm
-	killall rsync
-	killall orchestrator
-	ETCDCTL_API=3 etcdctl --endpoints http://$ETCD_IP:2379 del "" --from-key=true
-	mongo $MONGO_IP/emco --eval "db.resources.remove({})"
+        ns="privileged-lc-ns"
+        killall dcm
+        killall rsync
+        killall orchestrator
+        ETCDCTL_API=3 etcdctl --endpoints http://$ETCD_IP:2379 del "" --from-key=true
+        mongo $MONGO_IP/emco --eval "db.resources.remove({})"
         ssh $k221 -C "kubectl delete ns $ns;kubectl delete resourcebundlestate --all; kubectl delete csr --all"
         ssh $k231 -C "kubectl delete ns $ns;kubectl delete resourcebundlestate --all; kubectl delete csr --all"
-	sleep 2
-	echo "ATTENTION: Only deleting namespace $ns on the edge clusters..."
-	cd $EMCO_DIR/bin/orchestrator
-	./orchestrator >> log.txt 2>&1 &
-	sleep 2
-	cd $EMCO_DIR/bin/rsync
-	./rsync >> log.txt 2>&1 &
-	cd $EMCO_DIR/bin/dcm
-	./dcm >> log.txt 2>&1 &
+        sleep 2
+        echo "ATTENTION: Only deleting namespace $ns on the edge clusters..."
+        cd $EMCO_DIR/bin/orchestrator
+        ./orchestrator >> log.txt 2>&1 &
+        sleep 2
+        cd $EMCO_DIR/bin/rsync
+        ./rsync >> log.txt 2>&1 &
+        cd $EMCO_DIR/bin/dcm
+        ./dcm >> log.txt 2>&1 &
 }
