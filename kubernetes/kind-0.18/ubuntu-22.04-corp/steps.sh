3ade4e33d801b88aa68fa26b5e2f2c817c1b0f37
Igor DC <igor.duarte.cardoso@intel.com>
2023-04-14 15:35:58 -0700

    Final steps for kubectl

--- kubernetes/kind-0.18/ubuntu-22.04-corp/steps.sh
+++ kubernetes/kind-0.18/ubuntu-22.04-corp/steps.sh
@@ -39,3 +39,11 @@ sudo su
 service docker restart
 exit
 docker run hello-world
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin
+
+kind create cluster
+kubectl cluster-info --context kind-kind
+kubectl get pods -A
